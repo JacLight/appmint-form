94e182c84a2f8699e37c651702cc10ce9b4d99ea
jaclight <imolewolede@gmail.com>
2025-03-02 18:52:17 -0600

    feat: Remove unused Thing component from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import { CollectionForm } from './library/form-view';
 import { CollectionTable } from './library/table-view';
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
-};
-
 export const AppmintForm = (props: { initData, rules?, schema, theme?, datatype?, id, onChange?: (path, value, data, files, error) => void }) => {
   const { initData, schema, theme, datatype, rules, id, onChange } = props;
   return <CollectionForm data={initData || {}} path={''} schema={schema} accessMode={''} theme={theme || {}} rules={rules} datatype={datatype} id={id} onChange={onChange} />
